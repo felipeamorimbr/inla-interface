a9242d2215dea28bf3b8abf879ba0c9a2f2ecf4b
felipeamorimbr <felipeamorimufmg@gmail.com>
2020-03-02 00:57:05 -0300

    failed to add packrat

--- functions/file_ext.R
+++
@@ -1,8 +0,0 @@
-#function from tools package that recive a character and returns the characters after the "."
-#used to returns extension from a file
-
-file_ext <- function (x) 
-{
-  pos <- regexpr("\\.([[:alnum:]]+)$", x)
-  ifelse(pos > -1L, substring(x, pos + 1L), "")
-}
