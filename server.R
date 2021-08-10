6a385ae40cbe9f0a4c8841ceedeafc366b537f39
Ítalo Ferreira <36010914+itff@users.noreply.github.com>
2021-08-10 17:42:18 -0300

    Update server.R

--- server.R
+++ server.R
@@ -19,4 +19,6 @@ server <- function(input, output, session) {
   source("modal/lm_random_modal.R", local = TRUE)
   #Spatial modal
   source("modal/spatial_modal.R", local = TRUE)
+  #Time series Model
+  source("modal/ts_modal.R", local = TRUE)
 }
